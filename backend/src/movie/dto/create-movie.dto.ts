13f6512e90c547e8e7e844faf7e5003a5035a800
Artyom Plyusch <sincerity158@gmail.com>
2023-04-23 03:45:27 +0300

    добавил dto для фильмов

---
+++ backend/src/movie/dto/create-movie.dto.ts
@@ -0,0 +1,11 @@
+export class CreateMovieDto {
+    readonly title: string;
+    readonly originalTitle?: string;
+    readonly ageRate: string;
+    readonly description: string;
+    readonly year: number;
+    readonly country: string;
+    readonly premierRussia: string;
+    readonly premier: string;
+    readonly img: string;
+}
