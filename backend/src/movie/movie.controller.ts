61cf9e8f46197f0d79d1021da17e01728381875d
Artyom Plyusch <sincerity158@gmail.com>
2023-04-23 03:45:42 +0300

    добавил 2 гет запроса в контроллер фильмов

--- backend/src/movie/movie.controller.ts
+++ backend/src/movie/movie.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Param} from '@nestjs/common';
+import {MovieService} from "./movie.service";
 
 @Controller('movie')
-export class MovieController {}
+export class MovieController {
+    constructor(private movieService: MovieService) {
+    }
+
+    @Get()
+    async getMovies() {
+        return this.movieService.getAllMovies();
+    }
+
+    @Get(':id')
+    async getMovie(@Param('id') id: number) {
+        return this.movieService.getMovie(id);
+    }
+}
