b139092e47c971b63d993c7805d339f723d60d15
Artyom Plyusch <sincerity158@gmail.com>
2023-04-23 03:46:23 +0300

    добавил функции на гет запросы, и создание фильма

--- backend/src/movie/movie.service.ts
+++ backend/src/movie/movie.service.ts
@@ -3,6 +3,7 @@ import { Movie } from "./movie.model";
 import { InjectModel } from "@nestjs/sequelize";
 import { People } from "../people/people.model";
 import { Genres } from "../genres/genres.model";
+import {CreateMovieDto} from "./dto/create-movie.dto";
 
 @Injectable()
 export class MovieService {
@@ -10,7 +11,16 @@ export class MovieService {
         @InjectModel(People) private peopleRepository: typeof People,
         @InjectModel(Genres) private genreRepository: typeof Genres) { }
     
-    
+    async createMovie(dto: CreateMovieDto) {
+        return await this.movieRepository.create(dto);
+    }
 
 
+    async getAllMovies() {
+        return this.movieRepository.findAll({include: {all: true}});
+    }
+
+    async getMovie(id: number) {
+        return this.movieRepository.findByPk(id, {include: {all: true}});
+    }
 }
