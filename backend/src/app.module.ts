e9673080f25904e59efc2b228a6ca4aac1d20953
Artyom Plyusch <sincerity158@gmail.com>
2023-04-26 04:48:56 +0300

    зарегал клиент модуль

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,8 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from "@nestjs/config";
-import { SequelizeModule } from "@nestjs/sequelize";
-import { ParseModule } from './parse/parse.module';
-import { MovieModule } from './movie/movie.module';
-import { PeopleModule } from './people/people.module';
-import { Movie } from "./movie/movie.model";
-import { People } from "./people/people.model";
-import { MoviePeople } from "./people/moviepeople.model";
-import { GenresModule } from './genres/genres.module';
-import { Genres } from "./genres/genres.model";
-import { MovieGenres } from "./genres/moviegenres.model";
-import { Images } from './imgs/imgs.model';
-import { ImagesModule } from './imgs/imgs.module';
+import {Module} from '@nestjs/common';
+import {ConfigModule} from "@nestjs/config";
+import {SequelizeModule} from "@nestjs/sequelize";
+import {ParseModule} from './parse/parse.module';
+import {ClientsModule, Transport} from "@nestjs/microservices";
 
 
 @Module({
@@ -21,21 +12,18 @@ import { ImagesModule } from './imgs/imgs.module';
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`
     }),
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      models: [Movie, People, MoviePeople, Genres, MovieGenres, Images],
-      autoLoadModels: true
-    }),
-    ParseModule,
-    MovieModule,
-    PeopleModule,
-    GenresModule,
-    ImagesModule
+    ClientsModule.register([{
+      name: 'DB_SERVICE',
+      transport: Transport.RMQ,
+      options: {
+        urls: [`amqp://localhost:5672`],
+        queue: 'db_queue',
+        queueOptions: {
+          durable: true
+        }
+      }
+    }]),
+    ParseModule
   ]
 })
 export class AppModule { }
