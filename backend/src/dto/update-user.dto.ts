8a2900ce9abd7689d71f240f1341395c1ecafca0
Artyom Plyusch <sincerity158@gmail.com>
2023-05-13 02:36:47 +0300

    документация

--- backend/src/dto/update-user.dto.ts
+++ backend/src/dto/update-user.dto.ts
@@ -1,15 +1,25 @@
-import { IsNotEmpty, IsNumber, IsString, Length } from "class-validator";
+import {IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString, Length} from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class UpdateUserDto {
+  @ApiProperty({example: 'newemail@mail.ru', description: 'Почта'})
+  @IsOptional()
+  @IsEmail({},{message: 'Неверная почта'})
+  readonly email?: string;
 
-  @ApiProperty({ example: '1', description: "Unique user ID" })
-  @IsNotEmpty({ message: "Id should not be empty" })
-  @IsNumber({}, { message: 'Id must be string' })
-  readonly id: number;
+  @ApiProperty({ example: 'lkfahsje', description: "Пароль" })
+  @IsOptional()
+  @IsString({ message: 'Пароль должен быть строкой' })
+  @Length(4, 16, { message: 'Длина пароля от 4 до 16 символов' })
+  readonly password?: string;
 
-  @ApiProperty({ example: 'lkfahsje', description: "Password" })
-  @IsString({ message: 'Password must be string' })
-  @Length(4, 16, { message: 'Password length must be between 4 and 16 symbols' })
-  readonly password: string;
+  @ApiProperty({example: '+79003005025', description: 'Номер телефона'})
+  @IsOptional()
+  @IsString({message: 'Должен быть строкой'})
+  readonly phoneNumber?: string;
+
+  @ApiProperty({example: 'activationlink', description: 'Ссылка активации'})
+  @IsOptional()
+  readonly activationLink?: string;
+
 }
