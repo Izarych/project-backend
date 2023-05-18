004cf51e3e73e546872e472ec62356a5aa484468
Федотова Евгения <ev.fed1500@gmail.com>
2023-05-18 16:34:10 +0600

    поменяла тест контроллера фильмов

--- workwithdb/src/movie/test/movie.controller.spec.ts
+++ workwithdb/src/movie/test/movie.controller.spec.ts
@@ -16,6 +16,7 @@ describe('MovieController', () => {
       "title": "1+1",
       "originalTitle": "Intouchables",
       "ageRate": 16,
+      "country": "Франция",
       "rate": 8.8,
       "genres": [
         {
@@ -54,6 +55,7 @@ describe('MovieController', () => {
       "title": "Джентльмены",
       "originalTitle": "The Gentlemen",
       "ageRate": 18,
+      "country": "Великобритания, США",
       "rate": 8.5,
       "genres": [
         {
@@ -96,15 +98,40 @@ describe('MovieController', () => {
         {
           provide: MovieService,
           useValue: {
-            getAllMovies: jest.fn(),
-            getMovieByAgeRate: jest.fn(),
-            getMovieByCountry: jest.fn(),
-            getMovieByGenre: jest.fn(),
-            getMovieByRate: jest.fn(),
-            getMovie: jest.fn(),
-            getMoviePeople: jest.fn(),
-            getMovieGenres: jest.fn(),
-            getMovieByTitle: jest.fn(),
+            getAllMovies: jest.fn(() => movies),
+
+            getMovieByAgeRate: jest.fn((ageRate: number) =>
+              movies.filter((movie) => movie.ageRate <= ageRate)),
+
+            getMovieByCountry: jest.fn((countries: string) =>
+              movies.filter((movie) => movie.country === countries)
+            ),
+
+            getMovieByGenre: jest.fn((genre: string) =>
+              movies.filter((movie) =>
+                movie.genres.find((item) => item.genre === genre)
+              )
+            ),
+
+            getMovieByRate: jest.fn((rate: number) =>
+              movies.filter((movie) => movie.rate >= rate)
+            ),
+
+            getMovie: jest.fn((id: number) =>
+              movies.find((movie) => movie.id === id)
+            ),
+
+            getMoviePeople: jest.fn((id: number) =>
+              movies.find((movie) => movie.id === id).people
+            ),
+
+            getMovieGenres: jest.fn((id: number) =>
+              movies.find((movie) => movie.id === id).genres
+            ),
+
+            getMovieByTitle: jest.fn((title: string) =>
+              movies.find((movie) => movie.title === title)
+            ),
           },
         },
       ],
@@ -126,15 +153,15 @@ describe('MovieController', () => {
   describe('getMovies', () => {
     describe('when getMovies called', () => {
 
-      let response;
+      let response, spy;
 
       beforeEach(async () => {
-        jest.spyOn(movieService, 'getAllMovies').mockResolvedValue(movies as Movie[]);
+        spy = jest.spyOn(movieService, 'getAllMovies');
         response = await movieController.getMovies();
       });
 
       it('should call app service', async () => {
-        expect(movieService.getAllMovies).toHaveBeenCalled();
+        expect(spy).toHaveBeenCalled();
       });
 
       it('should return array containing all movies', async () => {
@@ -146,19 +173,19 @@ describe('MovieController', () => {
   describe('getMovieByAgeRate', () => {
     describe('when getMovieByAgeRate called', () => {
 
-      let response;
+      let response, spy;
       const ageRate = 16;
 
       beforeEach(async () => {
-        jest.spyOn(movieService, 'getMovieByAgeRate').mockResolvedValue([movies[0]] as Movie[]);
+        spy = jest.spyOn(movieService, 'getMovieByAgeRate');
         response = await movieController.getMovieByAgeRate(ageRate);
       });
 
       it('should call app service with age rate', async () => {
-        expect(movieService.getMovieByAgeRate).toHaveBeenCalledWith(ageRate);
+        expect(spy).toHaveBeenCalledWith(ageRate);
       });
 
-      it('should return array containing movies that match the age rate', async () => {
+      it('should return movies whose age rate does not exceed the specified age rate', async () => {
         expect(response).toEqual([movies[0]]);
       });
     });
@@ -167,16 +194,16 @@ describe('MovieController', () => {
   describe('getMovieByCountry', () => {
     describe('when getMovieByCountry called', () => {
 
-      let response;
+      let response, spy;
       const country = 'Франция';
 
       beforeEach(async () => {
-        jest.spyOn(movieService, 'getMovieByCountry').mockResolvedValue([movies[0]] as Movie[]);
+        spy = jest.spyOn(movieService, 'getMovieByCountry');
         response = await movieController.getMovieByCountry(country);
       });
 
       it('should call app service with country', async () => {
-        expect(movieService.getMovieByCountry).toHaveBeenCalledWith(country);
+        expect(spy).toHaveBeenCalledWith(country);
       });
 
       it('should return array containing movies that match the country', async () => {
@@ -189,20 +216,20 @@ describe('MovieController', () => {
   describe('getMovieByGenre', () => {
     describe('when getMovieByGenre called', () => {
 
-      let response;
+      let response, spy;
       const genre = 'комедия';
 
       beforeEach(async () => {
-        jest.spyOn(movieService, 'getMovieByGenre').mockResolvedValue([movies[0], movies[1]] as Movie[]);
+        spy = jest.spyOn(movieService, 'getMovieByGenre');
         response = await movieController.getMovieByGenre(genre);
       });
 
       it('should call app service with genre', async () => {
-        expect(movieService.getMovieByGenre).toHaveBeenCalledWith(genre);
+        expect(spy).toHaveBeenCalledWith(genre);
       });
 
       it('should return array containing movies that match the genre', async () => {
-        expect(response).toEqual([movies[0], movies[1]]);
+        expect(response).toEqual(movies)
       });
 
     });
@@ -211,20 +238,20 @@ describe('MovieController', () => {
   describe('getMovieByRate', () => {
     describe('when getMovieByRate called', () => {
 
-      let response;
+      let response, spy;
       const rate = 8.5;
 
       beforeEach(async () => {
-        jest.spyOn(movieService, 'getMovieByRate').mockResolvedValue([movies[1]] as Movie[]);
+        spy = jest.spyOn(movieService, 'getMovieByRate');
         response = await movieController.getMovieByRate(rate);
       });
 
       it('should call app service with rate', async () => {
-        expect(movieService.getMovieByRate).toHaveBeenCalledWith(rate);
+        expect(spy).toHaveBeenCalledWith(rate);
       });
 
-      it('should return array containing movies that match the rate', async () => {
-        expect(response).toEqual([movies[1]]);
+      it('should return movies whose rate is not less than the given rate', async () => {
+        expect(response).toEqual(movies);
       });
 
     });
@@ -233,16 +260,16 @@ describe('MovieController', () => {
   describe('getMovie', () => {
     describe('when getMovie called', () => {
 
-      let response;
+      let response, spy;
       const id = 2;
 
       beforeEach(async () => {
-        jest.spyOn(movieService, 'getMovie').mockResolvedValue(movies[1] as Movie);
+        spy = jest.spyOn(movieService, 'getMovie');
         response = await movieController.getMovie(id);
       });
 
       it('should call app service with id', async () => {
-        expect(movieService.getMovie).toHaveBeenCalledWith(id);
+        expect(spy).toHaveBeenCalledWith(id);
       });
 
       it('should return movie with sent id', async () => {
@@ -255,16 +282,16 @@ describe('MovieController', () => {
   describe('getMoviePeople', () => {
     describe('when getMoviePeople called', () => {
 
-      let response;
+      let response, spy;
       const id = 2;
 
       beforeEach(async () => {
-        jest.spyOn(movieService, 'getMoviePeople').mockResolvedValue(movies[1].people as People[]);
+        spy = jest.spyOn(movieService, 'getMoviePeople');
         response = await movieController.getMoviePeople(id);
       });
 
       it('should call app service with id', async () => {
-        expect(movieService.getMoviePeople).toHaveBeenCalledWith(id);
+        expect(spy).toHaveBeenCalledWith(id);
       });
 
       it('should return people', async () => {
@@ -277,16 +304,16 @@ describe('MovieController', () => {
   describe('getMovieGenres', () => {
     describe('when getMovieGenres called', () => {
 
-      let response;
+      let response, spy;
       const id = 2;
 
       beforeEach(async () => {
-        jest.spyOn(movieService, 'getMovieGenres').mockResolvedValue(movies[1].genres as Genres[]);
+        spy = jest.spyOn(movieService, 'getMovieGenres').mockResolvedValue(movies[1].genres as Genres[]);
         response = await movieController.getMovieGenres(id);
       });
 
       it('should call app service with id', async () => {
-        expect(movieService.getMovieGenres).toHaveBeenCalledWith(id);
+        expect(spy).toHaveBeenCalledWith(id);
       });
 
       it('should return genres', async () => {
@@ -299,16 +326,16 @@ describe('MovieController', () => {
   describe('getMovieByTitle', () => {
     describe('when getMovieByTitle called', () => {
 
-      let response;
+      let response, spy;
       const title = 'Джентльмены';
 
       beforeEach(async () => {
-        jest.spyOn(movieService, 'getMovieByTitle').mockResolvedValue(movies[1] as Movie);
+        spy = jest.spyOn(movieService, 'getMovieByTitle');
         response = await movieController.getMovieByTitle(title);
       });
 
       it('should call app service with title', async () => {
-        expect(movieService.getMovieByTitle).toHaveBeenCalledWith(title);
+        expect(spy).toHaveBeenCalledWith(title);
       });
 
       it('should return movie with sent title', async () => {
